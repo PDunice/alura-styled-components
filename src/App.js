86f8d4a75973a3fba51f2404b9b99663663914a7
PDunice <50148332+PDunice@users.noreply.github.com>
2022-02-14 10:12:43 -0300

    styled componnets
    fim do curso

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { temaClaro, temaEscuro } from "./Components/UI/temas";
 
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
+import { GlobalStyle } from "./Components/GlobalStyle";
+import { BtnTema, Icone } from "./Components/UI";
+import SwitcherTema from './Components/SwitcherTema'
 
 function App() {
+
+  const [tema, setTema] = useState(true);
+  const toogleTema = () => {
+    setTema((tema) => !tema)
+  }
   return (
     <>
-      <Cabecalho />
-      <Container />
+      <ThemeProvider theme={(tema)? temaClaro : temaEscuro}>
+        <GlobalStyle />
+        <BtnTema onClick={toogleTema}>
+          <SwitcherTema tema={tema}/>
+        </BtnTema>
+        <Cabecalho />
+        <Container />
+      </ThemeProvider>
     </>
   );
 }
